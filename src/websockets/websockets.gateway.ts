65f86ab3f072fc44de936f240da2b799127d1551
Daniel Lee <jhoon8903@gmail.com>
2023-11-12 00:18:22 +0900

    Nest JS RPG SOCKET SERVER

---
+++ src/websockets/websockets.gateway.ts
@@ -0,0 +1,32 @@
+// websockets.gateway.ts
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ path: '/chat' })
+export class WebsocketsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: string): void {
+    this.server.emit('message', payload);
+    console.log(payload);
+  }
+}
